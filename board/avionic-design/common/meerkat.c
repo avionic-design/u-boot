6a8cef5d571f2e14e583a66a42e52be9c637ecaf
Julian Scheel <julian@jusst.de>
2014-10-07 17:18:21 +0200

    Add support the Avionic Design Meerkat COM and Kein Baseboard
    This is minimal definition based on jetson tk1, the final baseboard
    will have a few more interfaces.
    
    Change-Id: I5df30569c743d4307742726fc0d2694da44cbb69
    Signed-off-by: Alban Bedel <alban.bedel@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/2612
    Reviewed-by: Julian Scheel <julian@jusst.de>

---
+++ board/avionic-design/common/meerkat.c
@@ -0,0 +1,30 @@
+/*
+ * (C) Copyright 2014, NVIDIA Corporation <www.nvidia.com>
+ * (C) Copyright 2015, Avionic Design <www.avionic-design.de>
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#include <common.h>
+#include <errno.h>
+#include <asm/arch/gpio.h>
+#include <asm/arch/pinmux.h>
+#include "pinmux-config-meerkat.h"
+
+/*
+ * Routine: pinmux_init
+ * Description: Do individual peripheral pinmux configs
+ */
+void pinmux_init(void)
+{
+	pinmux_set_tristate_input_clamping();
+
+	gpio_config_table(meerkat_gpio_inits,
+			  ARRAY_SIZE(meerkat_gpio_inits));
+
+	pinmux_config_pingrp_table(meerkat_pingrps,
+				   ARRAY_SIZE(meerkat_pingrps));
+
+	pinmux_config_drvgrp_table(meerkat_drvgrps,
+				   ARRAY_SIZE(meerkat_drvgrps));
+}
