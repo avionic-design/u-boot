7706e46f3247eb2776b9579e6077349709a6049f
Julian Scheel <julian@jusst.de>
2015-01-26 13:46:21 +0100

    meerkat: Add pcie init to common code
    Initialise the pcie controller when it is enabled in a boards configuration.
    The code is migrated from jetson board code.
    
    Change-Id: I823e648ae057d2b4ddbdc49101411e9c98a17cec
    Signed-off-by: Julian Scheel <julian@jusst.de>
    Reviewed-on: http://review.adnet.avionic-design.de/3305
    Reviewed-by: Nikolaus Schulz <nikolaus.schulz@avionic-design.de>

--- board/avionic-design/common/meerkat.c
+++ board/avionic-design/common/meerkat.c
@@ -7,6 +7,8 @@
 
 #include <common.h>
 #include <errno.h>
+#include <power/as3722.h>
+
 #include <asm/arch/gpio.h>
 #include <asm/arch/pinmux.h>
 #include "pinmux-config-meerkat.h"
@@ -25,3 +27,44 @@ void pinmux_init(void)
 	pinmux_config_drvgrp_table(meerkat_drvgrps,
 				   ARRAY_SIZE(meerkat_drvgrps));
 }
+
+#ifdef CONFIG_PCI_TEGRA
+int tegra_pcie_board_init(void)
+{
+	struct udevice *pmic;
+	int err;
+
+	err = as3722_init(&pmic, 4, 0x40);
+	if (err) {
+		error("failed to initialize AS3722 PMIC: %d\n", err);
+		return err;
+	}
+
+	err = as3722_sd_enable(pmic, 4);
+	if (err < 0) {
+		error("failed to enable SD4: %d\n", err);
+		return err;
+	}
+
+	err = as3722_sd_set_voltage(pmic, 4, 0x24);
+	if (err < 0) {
+		error("failed to set SD4 voltage: %d\n", err);
+		return err;
+	}
+
+	err = as3722_gpio_configure(pmic, 1, AS3722_GPIO_OUTPUT_VDDH |
+					     AS3722_GPIO_INVERT);
+	if (err < 0) {
+		error("failed to configure GPIO#1 as output: %d\n", err);
+		return err;
+	}
+
+	err = as3722_gpio_direction_output(pmic, 2, 1);
+	if (err < 0) {
+		error("failed to set GPIO#2 high: %d\n", err);
+		return err;
+	}
+
+	return 0;
+}
+#endif /* PCI */
